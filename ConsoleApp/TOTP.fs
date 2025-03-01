9e6f4c310a22a61827c539d5f1d09b5e34a324d8
Alex <alessandro.piccione.75@gmail.com>
2025-03-01 09:41:16 +0000

    create code with different culture

--- ConsoleApp/TOTP.fs
+++ ConsoleApp/TOTP.fs
@@ -3,6 +3,7 @@
 open System
 open Spectre.Console
 open System.Text
+open OtpNet
 
 let useDefaultIfEmpty value defaultValue =
     if String.IsNullOrWhiteSpace value then defaultValue else value
@@ -12,11 +13,49 @@ let askOrDefault (prompt) (defaultValue:string) =
     | "y" -> defaultValue
     | input -> input
 
-let GenerateCode (secretKey:string) =
+let rec GenerateCode (secretKey:string) =
     let bytes = ASCIIEncoding.ASCII.GetBytes(secretKey)
-    let totp = OtpNet.Totp(bytes)
+    let totp = Totp(bytes, 30, OtpHashMode.Sha256)
+
     let totpNumber = totp.ComputeTotp()
     AnsiConsole.MarkupLine $"TOTP: [yellow bold]{totpNumber}[/]"
+
+    System.Threading.Thread.Sleep(2_000)
+
+    // Is culture influencing hte TOTP ?
+    System.Threading.Thread.CurrentThread.CurrentCulture <- System.Globalization.CultureInfo("en-US")
+    System.Threading.Thread.CurrentThread.CurrentUICulture <- System.Globalization.CultureInfo("en-US")
+    let totpNumber = totp.ComputeTotp()
+    AnsiConsole.MarkupLine $"TOTP: [yellow bold]{totpNumber}[/]"
+
+
+    System.Threading.Thread.Sleep(10_000)
+
+    match AnsiConsole.Ask<string> "Continue (y/n)?" with
+    | "y" -> GenerateCode secretKey
+    | _ -> ()
+
+
+let rec VrifyCode (secretKey:string) =
+    let bytes = ASCIIEncoding.ASCII.GetBytes(secretKey)
+    let totp = Totp(bytes, 30, OtpHashMode.Sha256)
+
+
+    let code = AnsiConsole.Ask "Code:"
+    let matches = totp.VerifyTotp(code, [<Out>] timeStepMatched)
+
+    if matches then AnsiConsole.Markup $"Code matches"
+    else AnsiConsole.Markup $"[red]Code NOT matches[/]"
+
+    ()
+    //let totpNumber = totp.VerifyTotp DateTime.UtcNow 
+    //AnsiConsole.MarkupLine $"TOTP: [yellow bold]{totpNumber}[/]"
+    //System.Threading.Thread.Sleep(30_000)
+
+    //match AnsiConsole.Ask<string> "Continue (y/n)?" with
+    //| "y" -> GenerateCode secretKey
+    //| _ -> ()
+
 
 let CreateNewAuthentication () =
     AnsiConsole.MarkupLine "\n[aqua bold]# Generate TOTP #[/]\n"
